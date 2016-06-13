2760ccb203346bfd84f41f4a05ecd78ca33022f4
Malcolm Jack <inquisitorjax@gmail.com>
2016-06-13 11:45:57 +0200

    todo store item

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,20 +2,28 @@
  * Created by malcolmj on 6/6/2016.
  */
 export const STORAGE_KEY = 'Todo';
-//import {length, required, date} from 'aurelia-validatejs';
+import {length, required} from 'aurelia-validatejs';
 
 export default class TodoItem
 {
-  //NOTE: Currently get error with circular reference when trying to stringify via JSON
-  // @length({ minimum: 5, maximum: 25 })  description = '';
+  // @length({ minimum: 4, maximum: 25 })  description = '';
   // @required dueDate = '';
+  // isDone = false;
 
   constructor()
   {
-    this.description = "";
-    this.dueDate = "";
+    this.description = '';
+    this.dueDate = '';
     this.isDone = false;
   }
+}
 
-  
+export default class TodoStoreItem
+{
+  constructor(description, dueDate, isDone)
+  {
+    this.description = description;
+    this.dueDate = dueDate;
+    this.isDone = isDone;
+  }
 }
